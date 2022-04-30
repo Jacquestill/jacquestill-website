460dd951ca97f8463aac2a9ed8192c71ff2013fb
Jacquestill <89329719+Jacquestill@users.noreply.github.com>
2022-04-30 21:35:40 +0900

    Socials margin fix

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -5,23 +5,25 @@ import { AnimationOnScroll } from "react-animation-on-scroll";
 
 function HomePage() {
   return (
-    <div id="home" className=" container-fluid row home">
-      <div className="col-lg-9">
-        <AnimationOnScroll animateIn="animate__fadeIn">
-          <h1 className="home-title">Jacques Till</h1>
-        </AnimationOnScroll>
-        <AnimationOnScroll animateIn="animate__fadeIn">
-          <h3 className="home-subtitle sub-header">Full-Stack Developer</h3>
-        </AnimationOnScroll>
-        <AnimationOnScroll animateIn="animate__fadeIn">
-          <p className="home-content sub-header">
-            Helping people realize their techno dreams by creating websites that
-            are innovative and intuitive.
-          </p>
-        </AnimationOnScroll>
-      </div>
-      <div className="col-lg-2">
-        <img className="home-img" src={standing} alt="standing-logo" />
+    <div>
+      <div id="home" className=" container-fluid row home">
+        <div className="col-lg-9">
+          <AnimationOnScroll animateIn="animate__fadeIn">
+            <h1 className="home-title">Jacques Till</h1>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeIn">
+            <h3 className="home-subtitle sub-header">Full-Stack Developer</h3>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeIn">
+            <p className="home-content sub-header">
+              Helping people realize their techno dreams by creating websites
+              that are innovative and intuitive.
+            </p>
+          </AnimationOnScroll>
+        </div>
+        <div className="col-lg-2">
+          <img className="home-img" src={standing} alt="standing-logo" />
+        </div>
       </div>
       <Socials />
     </div>
